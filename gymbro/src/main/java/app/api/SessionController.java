ade8892b2b96771a442402b756312a7ddc479fa3
Rafal Janicki <bloorq@gmail.com>
2026-08-04 16:35:01 +0100

    scaffolding

--- gymbro/src/main/java/app/api/SessionController.java
+++ gymbro/src/main/java/app/api/SessionController.java
@@ -6,12 +6,19 @@ import app.model.Session;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SessionController {
 
     private static final String template = "Session %s";
     private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/sessions")
+    public String session() {
+        return "All Sessions";
+    }
 
     @GetMapping("/session")
     public Session session(@RequestParam(defaultValue = "last") String sessionName) {
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-// import {useState} from 'react'
+import {useEffect, useState} from 'react'
+
+type SessionProps = {
+    id?: number,
+    content?: string,
+};
 
 function App() {
+
+    const [session, setSession] = useState<SessionProps>({});
+
+    useEffect((): void => {
+        fetch("http://localhost:8080/session?sessionName=first")
+            .then(res => res.json())
+            .then(res => setSession(res));
+    }, []);
 
     return (
         <>
             <div>GymBro</div>
+            {session.content ?? "Loading..."}
         </>
     )
 }
 
-export default App
+export default App;
