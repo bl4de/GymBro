fcace5500f5e8b4b352bfa96bd7a278459c519ee
Rafal Janicki <bloorq@gmail.com>
2026-08-13 14:09:51 +0100

    refactoring

--- gymbro/src/main/java/app/api/SessionController.java
+++ gymbro/src/main/java/app/api/SessionController.java
@@ -3,25 +3,23 @@ package app.api;
 import java.util.concurrent.atomic.AtomicLong;
 
 import app.model.Session;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/sessions")
 @RestController
 public class SessionController {
 
     private static final String template = "Session %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/sessions")
+    @GetMapping("/")
     public String session() {
         return "All Sessions";
     }
 
-    @GetMapping("/session")
-    public Session session(@RequestParam(defaultValue = "last") String sessionName) {
+    @GetMapping("/{sessionName}")
+    public Session session(@PathVariable @RequestParam(defaultValue = "last") String sessionName) {
         return new Session(counter.incrementAndGet(), template.formatted(sessionName));
     }
 }
